17804f8c7d739446958c6f7ed3659ee160a5df00
Marc Zoomers <marczoomers@gmail.com>
2023-03-16 15:43:24 +0100

    Rearanged order of functions in interactive.py

--- Source/interactive.py
+++ Source/interactive.py
@@ -12,6 +12,134 @@ class FileFormatError(Exception): pass
 class noFileGivenError(Exception): pass
 
 
+def get_file_path(description: str, fileformat: any) -> str:
+    """A function to get the filepath of a selected file.
+
+    Args:
+        description (str): Description of the to be selected file format.
+        fileformat (any): Either a string of one specified file format or a list of file formats. e.g. "*.txt" or ["*.txt", "*.docx"] # noqa: E501
+
+    Returns:
+        str: The filepath of the selected file.
+    """
+    root = Tk()
+    root.withdraw()  # we don't want a full GUI, so keep the root window from appearing
+    root.iconbitmap(os.path.realpath(os.path.dirname(__file__)) + "\\support_files\\logo.ico")
+
+    filename = askopenfilename(filetypes=[(description, fileformat)])  # show an "Open" dialog box and return the path to the selected file # noqa: E501
+    if filename:
+        print("The following file was selected: \n" + filename)
+        return filename
+
+    return
+
+
+def readout_LAS_file(filename: str) -> o3d.cpu.pybind.geometry.PointCloud:
+    """A function to read a LAS/LAZ file and convert the contents into an Open3D format.
+    This makes it possible to use the Open3D tools on the LAS/LAZ files.
+
+    Args:
+        filename (str): A PATH to a LAS/LAZ file to be converted.
+
+    Returns:
+        o3d.cpu.pybind.geometry.PointCloud: An Open3D point cloud containing the contents of the LAS/LAZ file.
+    """
+    try:
+        if not filename:
+            raise noFileGivenError
+
+        las = laspy.read(filename)
+
+        # check if LAS file is in the correct format
+        if "<LasData(1.2, point fmt: <PointFormat(3," not in str(las):
+            raise FileFormatError
+
+        geom = o3d.geometry.PointCloud()
+
+        # Create an Open3d model that contains the points from the LAS/LAZ file.
+        pointData = np.stack([
+            las.X,
+            las.Y,
+            las.Z
+        ], axis=0).transpose((1, 0))
+        geom.points = o3d.utility.Vector3dVector((pointData * las.header.scales) + las.header.offsets)
+
+        # Assign the colours of the points to the Open3d model. Open3d only takes in colour values between 0 and 1, so therefore the colour values will be normalized accordingly. # noqa: E501
+        colourData = np.stack([
+            normalize_array(las.red, True),
+            normalize_array(las.green, True),
+            normalize_array(las.blue, True)
+        ], axis=0).transpose((1, 0))
+        geom.colors = o3d.utility.Vector3dVector(colourData)
+
+        # # It seems like Open3d does not accept the use gps as a variable. Find a way to give this to the ply file
+        # gpsData = np.stack([las.gps_time], axis=0).transpose((1, 0))
+        # geom.gps = o3d.utility.Vector2dVector(gpsData)
+
+        print("A " + str(geom)[:-1] + " was extracted from the given LAS/LAZ file.")
+        return geom
+    except FileNotFoundError:
+        print("Could not find a file on the given PATH,  please check if the PATH exists.")
+        exit()
+    except noFileGivenError:
+        print("No file was selected, script will not be stopped.")
+        return
+    except laspy.errors.LaspyException:
+        print("The framework could not handle this file, please check if the file is not corrupted and/or if it is a LAS/LAZ file.")  # noqa: E501
+        exit()
+    except FileFormatError:
+        print("The chosen LAS/LAZ file is not in the correct format or correct version. This file will not be used.")
+        exit()
+    except Exception as e:
+        print("An unforeseen error occurred. See below for details.")
+        print(type(e))
+        print(e)
+        exit()
+
+
+def normalize_array(inputArray: np.ndarray, isColour: bool = False) -> np.ndarray:
+    """A function made to normalize a NumPy ndarray.
+
+    Args:
+        inputArray (numpy.ndarray): A NumPy ndarray to normalize.
+        isColour (bool, optional): A boolean value to divide the ndarray to the LAS colour standards to 0. Defaults to False.  # noqa: E501
+
+    Raises:
+        TypeError: If the inputArray is not of the NumPy ndarray type, this error will be raised.
+
+    Returns:
+       numpy.ndarray: The normalized NumPy ndarray.
+    """
+    try:
+        if type(inputArray) is not np.ndarray: raise TypeError
+
+        if isColour is True: normalizedArray = inputArray / 65535
+        else: normalizedArray = (inputArray - np.min(inputArray)) / (np.max(inputArray) - np.min(inputArray))
+
+        return normalizedArray
+    except TypeError:
+        print("Given value is not the correct type; not a NumPy ndarray.")
+
+
+def open_point_cloud_editor(pcd: o3d.cpu.pybind.geometry.PointCloud) -> None:
+    """A function to open a window that allows the point clouds to be cropped. The export of the cropped clouds are in PLY format.  # noqa: E501
+    It has to be noted that when exporting the cutouts to PLY format, Open3D will round off the colour values.
+    This results in somewhat distorted colors when converting them back to LAS format.
+
+    Args:
+        pcd (o3d.cpu.pybind.geometry.PointCloud): The point cloud to be edited.
+    """
+    print("\n")  # noqa: E303
+    print("1) Press 'Y' twice to align geometry with negative direction of y-axis")
+    print("2) Press 'K' to lock screen and to switch to selection mode")
+    print("3) Drag for rectangle selection,")
+    print("   or use ctrl + left click for polygon selection")
+    print("4) Press 'C' to get a selected geometry")
+    print("5) Press 'S' to save the selected geometry")
+    print("6) Press 'F' to switch to freeview mode")
+    o3d.visualization.draw_geometries_with_editing([pcd])
+
+
 def convert_ply_to_las(inputLasPath: str = None):
     """A function to convert a ply file to a LAS file, based on a given LAS input file.
     With this function the user is prompted to select a PLY file that will be converted to a LAS file.
@@ -97,134 +225,6 @@ def grid_subsampling(points: laspy.LasReader, voxel_size: int) -> list:
         last_seen += nb_pts_per_voxel[idx]
 
     return grid_candidate_center
-
-
-def get_file_path(description: str, fileformat: any) -> str:
-    """A function to get the filepath of a selected file.
-
-    Args:
-        description (str): Description of the to be selected file format.
-        fileformat (any): Either a string of one specified file format or a list of file formats. e.g. "*.txt" or ["*.txt", "*.docx"] # noqa: E501
-
-    Returns:
-        str: The filepath of the selected file.
-    """
-    root = Tk()
-    root.withdraw()  # we don't want a full GUI, so keep the root window from appearing
-    root.iconbitmap(os.path.realpath(os.path.dirname(__file__)) + "\\support_files\\logo.ico")
-
-    filename = askopenfilename(filetypes=[(description, fileformat)])  # show an "Open" dialog box and return the path to the selected file # noqa: E501
-    if filename:
-        print("The following file was selected: \n" + filename)
-        return filename
-
-    return
-
-
-def normalize_array(inputArray: np.ndarray, isColour: bool = False) -> np.ndarray:
-    """A function made to normalize a NumPy ndarray.
-
-    Args:
-        inputArray (numpy.ndarray): A NumPy ndarray to normalize.
-        isColour (bool, optional): A boolean value to divide the ndarray to the LAS colour standards to 0. Defaults to False.  # noqa: E501
-
-    Raises:
-        TypeError: If the inputArray is not of the NumPy ndarray type, this error will be raised.
-
-    Returns:
-       numpy.ndarray: The normalized NumPy ndarray.
-    """
-    try:
-        if type(inputArray) is not np.ndarray: raise TypeError
-
-        if isColour is True: normalizedArray = inputArray / 65535
-        else: normalizedArray = (inputArray - np.min(inputArray)) / (np.max(inputArray) - np.min(inputArray))
-
-        return normalizedArray
-    except TypeError:
-        print("Given value is not the correct type; not a NumPy ndarray.")
-
-
-def readout_LAS_file(filename: str) -> o3d.cpu.pybind.geometry.PointCloud:
-    """A function to read a LAS/LAZ file and convert the contents into an Open3D format.
-    This makes it possible to use the Open3D tools on the LAS/LAZ files.
-
-    Args:
-        filename (str): A PATH to a LAS/LAZ file to be converted.
-
-    Returns:
-        o3d.cpu.pybind.geometry.PointCloud: An Open3D point cloud containing the contents of the LAS/LAZ file.
-    """
-    try:
-        if not filename:
-            raise noFileGivenError
-
-        las = laspy.read(filename)
-
-        # check if LAS file is in the correct format
-        if "<LasData(1.2, point fmt: <PointFormat(3," not in str(las):
-            raise FileFormatError
-
-        geom = o3d.geometry.PointCloud()
-
-        # Create an Open3d model that contains the points from the LAS/LAZ file.
-        pointData = np.stack([
-            las.X,
-            las.Y,
-            las.Z
-        ], axis=0).transpose((1, 0))
-        geom.points = o3d.utility.Vector3dVector((pointData * las.header.scales) + las.header.offsets)
-
-        # Assign the colours of the points to the Open3d model. Open3d only takes in colour values between 0 and 1, so therefore the colour values will be normalized accordingly. # noqa: E501
-        colourData = np.stack([
-            normalize_array(las.red, True),
-            normalize_array(las.green, True),
-            normalize_array(las.blue, True)
-        ], axis=0).transpose((1, 0))
-        geom.colors = o3d.utility.Vector3dVector(colourData)
-
-        # # It seems like Open3d does not accept the use gps as a variable. Find a way to give this to the ply file
-        # gpsData = np.stack([las.gps_time], axis=0).transpose((1, 0))
-        # geom.gps = o3d.utility.Vector2dVector(gpsData)
-
-        print("A " + str(geom)[:-1] + " was extracted from the given LAS/LAZ file.")
-        return geom
-    except FileNotFoundError:
-        print("Could not find a file on the given PATH,  please check if the PATH exists.")
-        exit()
-    except noFileGivenError:
-        print("No file was selected, script will not be stopped.")
-        return
-    except laspy.errors.LaspyException:
-        print("The framework could not handle this file, please check if the file is not corrupted and/or if it is a LAS/LAZ file.")  # noqa: E501
-        exit()
-    except FileFormatError:
-        print("The chosen LAS/LAZ file is not in the correct format or correct version. This file will not be used.")
-        exit()
-    except Exception as e:
-        print("An unforeseen error occurred. See below for details.")
-        print(type(e))
-        print(e)
-        exit()
-
-
-def open_point_cloud_editor(pcd: o3d.cpu.pybind.geometry.PointCloud) -> None:
-    """A function to open a window that allows the point clouds to be cropped. The export of the cropped clouds are in PLY format.  # noqa: E501
-    It has to be noted that when exporting the cutouts to PLY format, Open3D will round off the colour values.
-    This results in somewhat distorted colors when converting them back to LAS format.
-
-    Args:
-        pcd (o3d.cpu.pybind.geometry.PointCloud): The point cloud to be edited.
-    """
-    print("\n")  # noqa: E303
-    print("1) Press 'Y' twice to align geometry with negative direction of y-axis")
-    print("2) Press 'K' to lock screen and to switch to selection mode")
-    print("3) Drag for rectangle selection,")
-    print("   or use ctrl + left click for polygon selection")
-    print("4) Press 'C' to get a selected geometry")
-    print("5) Press 'S' to save the selected geometry")
-    print("6) Press 'F' to switch to freeview mode")
-    o3d.visualization.draw_geometries_with_editing([pcd])
 
 
 if __name__ == "__main__":
