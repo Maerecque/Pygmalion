de67ffa815181d50bae23f3a3fc9bcd2c93ba52d
Marc Zoomers <marczoomers@gmail.com>
2023-03-22 13:22:29 +0100

    Added new parameters to batchRunner.py for more customization

--- Source/batchRunner.py
+++ Source/batchRunner.py
@@ -13,10 +13,12 @@ class emptyPointCloudError(Exception): pass
 
 def batch_running(
     input_list: list,
+    voxel_size: float = 0.05,
     radius_nb_points: int = 10,
     radius_radius: float = 0.1,
     statistical_nb_neighbors: int = 20,
     statistical_std_ratio: float = 2.0,
+    visualize_noise: bool = False,
     dbscan_eps: float = 0.1,
     dbscan_min_sample: int = 20,
     dbscan_keep_only_labels: bool = True,
@@ -27,10 +29,12 @@ def batch_running(
 
     Args:
         input_list (list): List to be used as input, contains path locations of files to be scanned.
+        voxel_size (float, optional): Distance between points that is allowed. Defaults to 0.05.
         radius_nb_points (int, optional): nb_points hyperparameter for the radius noise remover function. Defaults to 10.
         radius_radius (float, optional): radius hyperparameter for the radius noise remover function. Defaults to 0.1.
         statistical_nb_neighbors (int, optional): nb_neighbors hyperparameter for the statistical noise remover function. Defaults to 20.
         statistical_std_ratio (float, optional): std_ratio hyperparameter for the statistical noise remover function. Defaults to 2.0.
+        visualize_noise (bool, optional): Wether to visualize the noise that will be removed from the point clouds. Defaults to False.
         dbscan_eps (float, optional): eps hyperparameter for the db scan. Defaults to 0.1.
         dbscan_min_sample (int, optional): min_sample hyperparameter for the db scan. Defaults to 20.
         dbscan_keep_only_labels (bool, optional): Whether to keep only the labels from the dbscan. Defaults to True.
@@ -41,9 +45,14 @@ def batch_running(
         try:
             print(item)
             pcd = readout_LAS_file(item)
-            pcd = grid_subsampling(pcd)
+            pcd = grid_subsampling(pcd, voxel_size)
             print("Doing radius")
-            pcd_radius = remove_noise_radius(pcd, nb_points=radius_nb_points, radius=radius_radius)
+            pcd_radius = remove_noise_radius(
+                pcd,
+                visualize_noise,
+                nb_points=radius_nb_points,
+                radius=radius_radius
+            )
 
             # Check if the point cloud is empty after the noise has been removed.
             if len(pcd_radius.points) == 0:
@@ -64,7 +73,12 @@ def batch_running(
 
         try:
             print("Doing statistical")
-            pcd_statistical = remove_noise_statistical(pcd, nb_neighbors=statistical_nb_neighbors, std_ratio=statistical_std_ratio)
+            pcd_statistical = remove_noise_statistical(
+                pcd,
+                visualize_noise,
+                nb_neighbors=statistical_nb_neighbors,
+                std_ratio=statistical_std_ratio
+            )
 
             # Check if the point cloud is empty after the noise has been removed.
             if len(pcd_statistical.points) == 0:
@@ -106,13 +120,15 @@ if __name__ == "__main__":
     ]
 
     batch_running(
-        file_list,
-        dbscan_min_sample=5,
-        dbscan_eps=0.05,
+        file_list_hand_scans,
+        voxel_size=0.01,
+        dbscan_min_sample=20,
+        dbscan_eps=0.1,
         radius_nb_points=10,
         radius_radius=0.1,
         statistical_nb_neighbors=20,
         statistical_std_ratio=2.0,
+        visualize_noise=True,
         dbscan_keep_no_labels=False,
         dbscan_keep_only_labels=True,
         dbscan_visualize_all=False
