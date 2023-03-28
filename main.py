1d86ae9facd1313bbe0ceaae41479f3067826cf6
Marc Zoomers <marczoomers@gmail.com>
2023-03-28 10:45:42 +0200

    Removed print thingy in main.py about usage of large files 👾

--- main.py
+++ main.py
@@ -17,8 +17,6 @@ from Source.pointCloudEditor import open_point_cloud_editor
 
 
 if __name__ == "__main__":
-    print("Be aware that files that are created with this application cannot be used again with this application.")
-    print("NOTE: This application is not suited for very large files, try to use files that are 350MB or smaller. \n Unless you are running on a high-end computer.")  # noqa: E501
     file_name = get_file_path("LAS and LAZ files", ["*.las", "*.laz"])
     pcd = readout_LAS_file(file_name)
 
