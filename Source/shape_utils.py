4f1f3f70f48c1d3f4e51384ed35295eae9e1b03b
Marc Zoomers <marczoomers@gmail.com>
2023-04-17 14:45:18 +0200

    Added keep_points_in_view() function to shape_utils, but it also don't work yet ⚙

--- Source/shape_utils.py
+++ Source/shape_utils.py
@@ -1,4 +1,5 @@
 import open3d as o3d
+import numpy as np
 
 
 def find_alpha_shapes(point_cloud: o3d.cpu.pybind.geometry.PointCloud) -> o3d.cpu.pybind.geometry.TriangleMesh:
@@ -54,9 +55,45 @@ def find_outside_pointcloud(point_cloud: o3d.cpu.pybind.geometry.PointCloud) -> 
             if point.all() == vertex.all():
                 is_inside = True
         if not is_inside:
-            print(outer_point_found)
+            print("outer_point_found")
             exterior_points.append(point)
 
     print(exterior_points)
 
     return exterior_points
+
+
+def keep_points_in_view(point_cloud: o3d.cpu.pybind.geometry.PointCloud) -> o3d.cpu.pybind.geometry.PointCloud:
+    """_summary_
+    I still don't know how to get this function to work.
+
+    Args:
+        point_cloud (o3d.cpu.pybind.geometry.PointCloud): _description_
+
+    Returns:
+        o3d.cpu.pybind.geometry.PointCloud: _description_
+    """
+    diameter = np.linalg.norm(
+        np.asarray(point_cloud.get_max_bound()) - np.asarray(point_cloud.get_min_bound())
+    )
+
+    print(point_cloud.get_max_bound())
+
+    # Mostly it's just this camera feature that I just don't understand. I might look back at this another day
+    camera = [0, 0, diameter]
+    radius = diameter * 100
+
+    # This gives a bit of information about the function, but not enough to fricking understand it or atleast get how to get Camera to do what I want.
+    # Maybe I should just look at the source code of the function, because ChatGPT is not helping me at all.
+    help(o3d.cpu.pybind.geometry.PointCloud.hidden_point_removal)
+
+    print("Get all points that are visible from given view point")
+    _, pt_map = point_cloud.hidden_point_removal(camera, radius)
+
+    # print(_)
+    # print(type(_))
+
+    # o3d.visualization.draw_geometries([pt_map])
+
+    pcd = point_cloud.select_by_index(pt_map)
+    o3d.visualization.draw_geometries([pcd])
