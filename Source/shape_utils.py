9b2eaadaf7354b2b09513ca1940e6ef5597ca34d
Marc Zoomers <marczoomers@gmail.com>
2023-05-02 14:51:38 +0200

    Yooo, got segment_plane() to work 🎉

--- Source/shape_utils.py
+++ Source/shape_utils.py
@@ -123,3 +123,46 @@ def detect_planar_patches(point_cloud: o3d.cpu.pybind.geometry.PointCloud) -> o3
 
     # Visualize the point cloud with the segmented planes
     o3d.visualization.draw_geometries([point_cloud, *planes])
+
+
+def segment_plane(
+    point_cloud: o3d.cpu.pybind.geometry.PointCloud,
+    visualize_plane: bool = False,
+) -> o3d.cpu.pybind.geometry.PointCloud:
+    """A function to extract a plane from a point cloud.
+    Eyo this actually works, I'm so happy with this. I'm gonna use this for the next step in the pipeline.
+    Maybe not exactly as I expected, but I can work with this. 🤩
+
+    Args:
+        point_cloud (o3d.cpu.pybind.geometry.PointCloud): The point cloud to be processed.
+        visualize_plane (bool, optional): A boolean to determine whether or not to visualize the extracted plane. Defaults to False.
+
+    Returns:
+        o3d.cpu.pybind.geometry.PointCloud: The extracted plane in point cloud format.
+    """
+    # Define the parameters for plane segmentation
+    distance_threshold = 0.01
+    ransac_n = 3
+    num_iterations = 10000
+
+    # Use RANSAC to segment the point cloud into planes
+    plane_model, inliers = point_cloud.segment_plane(
+        distance_threshold=distance_threshold,
+        ransac_n=ransac_n,
+        num_iterations=num_iterations
+    )
+
+    # Extract the points at the specified indices in a for loop
+    extracted_points = []
+    for index in inliers:
+        extracted_points.append(point_cloud.points[index])
+
+    # Create a new point cloud from the extracted points
+    extracted_pcd = o3d.geometry.PointCloud()
+    extracted_pcd.points = o3d.utility.Vector3dVector(extracted_points)
+
+    if visualize_plane:
+        # Visualize the extracted plane
+        o3d.visualization.draw_geometries([extracted_pcd])
+
+    return extracted_pcd
