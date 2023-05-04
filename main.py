0fd9ada963219e8e11110ec4dd4ee631fc6f8ac6
Marc Zoomers <marczoomers@gmail.com>
2023-05-04 14:37:59 +0200

    Added expand_plane function to be under the DBScan function

--- main.py
+++ main.py
@@ -28,13 +28,6 @@ if __name__ == "__main__":
     term_size = os.get_terminal_size()
     print(u'\u2500' * term_size.columns)
 
-    # Yeees, my genius is sometimes frightening.
-    plane_from_pcd = expand_plane(pcd)
-    open_point_cloud_editor(plane_from_pcd)
-
-    # Yeah I know this value is not used, but I'll get to it later
-    plane_from_pcd = segment_plane(pcd)  # noqa: E303
-
     if pcd is not None:
         pcd = grid_subsampling(pcd, 0.05)
         pcd_stat = remove_noise_statistical(pcd, True)
@@ -51,6 +44,12 @@ if __name__ == "__main__":
 
         open_point_cloud_editor(pcd_combined)
 
+        # Yeees, my genius is sometimes frightening.
+        plane_from_pcd = expand_plane(pcd)
+        open_point_cloud_editor(plane_from_pcd)
+
+
+    # ____________________________ PLY MODULE ____________________________ # noqa: E303
     # When the point cloud alterations are done, the point cloud is saved as a PLY file or no LAS file is given, this part will start.
     print("\n")
     print("Starting PLY module")
