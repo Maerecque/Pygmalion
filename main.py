4ff51085f227331d36954f7877467ee9ba563ae9
Marc Zoomers <marczoomers@gmail.com>
2023-05-16 12:20:19 +0200

    Commented out this whole section for testing purposes

--- main.py
+++ main.py
@@ -31,33 +31,40 @@ if __name__ == "__main__":
     if pcd is not None:
         pcd = grid_subsampling(pcd, 0.05)
         pcd_stat = remove_noise_statistical(pcd, True)
-        pcd_cluster = pointcloud_dbscan(
-            pcd_stat,
-            eps=0.1,
-            min_samples=20,
-            keep_no_labels=True,
-            visualize_only_labels=True,
-            metric="cityblock"
-        )
+        # # Make a DBScan cluster of the point cloud.
+        # pcd_cluster = pointcloud_dbscan(
+        #     pcd_stat,
+        #     eps=0.03,
+        #     min_samples=10,
+        #     keep_no_labels=True,
+        #     visualize_only_labels=True,
+        #     metric="chebyshev"
+        # )
 
-        pcd_combined = combine_point_cloud(remove_noise_statistical(pcd, False, print_removal_amount=False), pcd_cluster)
+        # # Keep only points that were not labeled as noise.
+        # pcd_combined = combine_point_cloud(pcd_stat, pcd_cluster)
+        # open_point_cloud_editor(pcd_combined)
 
-        open_point_cloud_editor(pcd_combined)
+        # # Find planes in the point cloud manually.
+        # # Yes, my genius is sometimes frightening.
+        # pcd_planes = find_plane_module_manual(pcd_combined)
 
-        # Yeees, my genius is sometimes frightening.
-        plane_from_pcd = find_plane_module(pcd)
-        open_point_cloud_editor(plane_from_pcd)
+        # # Repair the point cloud with the planes found.
+        # # With these parameters, just try higher NN and depth and lower quantile value (and maybe scale)
+        # mesh = repair_point_cloud_module(pcd_planes, visualize=True, kdtree_max_nn=100, depth=13, quantile_value=0.01, scale=2.2)
 
-        # With these parameters, just try higher NN and depth and lower quantile value (and maybe scale)
-        repair_point_cloud_module(pcd, visualize=True, kdtree_max_nn=100, depth=13, quantile_value=0.01, scale=2.2)
+        # # Transform the mesh back to a point cloud.
+        # transformed_pcd = transform_mesh_to_pcd(mesh, pcd_planes)
+
+        # open_point_cloud_editor(transformed_pcd)
 
 
-    # ____________________________ PLY MODULE ____________________________ # noqa: E303
-    # When the point cloud alterations are done, the point cloud is saved as a PLY file or no LAS file is given, this part will start.
-    print("\n")
-    print("Starting PLY module")
 
-    # Print a nice line over the width of the terminal
-    print(u'\u2500' * term_size.columns)
+    # # ____________________________ PLY MODULE ____________________________ # noqa: E303
+    # # When the point cloud alterations are done, the point cloud is saved as a PLY file or no LAS file is given, this part will start.
+    # print("\n Starting PLY module")
 
-    convert_ply_to_las(file_name)
+    # # Print a nice line over the width of the terminal
+    # print(u'\u2500' * term_size.columns)
+
+    # convert_ply_to_las(file_name)
