aa3dd9233ca0d09292ee38b93b22536e9602d39b
Marc Zoomers <marczoomers@gmail.com>
2023-05-22 11:17:45 +0200

    ** BIG CHANGE** Removed a lot of DBSCAN code here to make main readable... 🚧

--- main.py
+++ main.py
@@ -4,21 +4,22 @@ import os
 # This line is needed so the scripts from the source folder are imported correctly without the need of an __init__ file.
 sys.path.insert(0, os.path.realpath(os.path.dirname(__file__)) + '\\Source')
 
-from Source.dbscanPointCloud import pointcloud_dbscan
+# from Source.dbscanPointCloud import pointcloud_dbscan
 from Source.fileHandler import (
-    convert_ply_to_las,
+    # convert_ply_to_las,
     get_file_path,
-    readout_LAS_file)
+    readout_LAS_file
+)
 from Source.pointCloudAltering import (
     grid_subsampling,
-    remove_noise_radius,
     remove_noise_statistical,
-    combine_point_cloud)
+    # combine_point_cloud
+)
 from Source.pointCloudEditor import open_point_cloud_editor
 from Source.shape_utils import (
-    find_plane_module_manual,
     repair_point_cloud_module,
-    transform_mesh_to_pcd)
+    transform_mesh_to_pcd
+)
 import Source.zigZagRansac as zzr
 
 
@@ -26,9 +27,9 @@ if __name__ == "__main__":
     file_name = get_file_path("LAS and LAZ files", ["*.las", "*.laz"])
     pcd = readout_LAS_file(file_name)
 
-    print("\n Starting DBScan module")
+    print("\n Starting PointCloud module")
 
-    # Print a nice line over the width of the terminal
+    # # Print a nice line over the width of the terminal
     term_size = os.get_terminal_size()
     print(u'\u2500' * term_size.columns)
 
@@ -47,33 +48,15 @@ if __name__ == "__main__":
 
         open_point_cloud_editor(plane_pointcloud, False)
 
+        # Repair the point cloud with the planes found.
+        # With these parameters, just try higher NN and depth and lower quantile value (and maybe scale)
+        mesh = repair_point_cloud_module(plane_pointcloud, visualize=True, kdtree_max_nn=100, depth=13, quantile_value=0.01, scale=2.2)
 
-        # # Make a DBScan cluster of the point cloud.
-        # pcd_cluster = pointcloud_dbscan(
-        #     pcd_stat,
-        #     eps=0.03,
-        #     min_samples=10,
-        #     keep_no_labels=True,
-        #     visualize_only_labels=True,
-        #     metric="chebyshev"
-        # )
+        # Transform the mesh back to a point cloud.
+        transformed_pcd = transform_mesh_to_pcd(mesh, plane_pointcloud)
 
-        # # Keep only points that were not labeled as noise.
-        # pcd_combined = combine_point_cloud(pcd_stat, pcd_cluster)
-        # open_point_cloud_editor(pcd_combined)
+        open_point_cloud_editor(transformed_pcd, False)
 
-        # # Find planes in the point cloud manually.
-        # # Yes, my genius is sometimes frightening.
-        # pcd_planes = find_plane_module_manual(pcd_combined)
-
-        # # Repair the point cloud with the planes found.
-        # # With these parameters, just try higher NN and depth and lower quantile value (and maybe scale)
-        # mesh = repair_point_cloud_module(pcd_planes, visualize=True, kdtree_max_nn=100, depth=13, quantile_value=0.01, scale=2.2)
-
-        # # Transform the mesh back to a point cloud.
-        # transformed_pcd = transform_mesh_to_pcd(mesh, pcd_planes)
-
-        # open_point_cloud_editor(transformed_pcd)
 
 
 
