6320abee63263d5d32ea237e866887aff52971f3
Marc Zoomers <marczoomers@gmail.com>
2023-08-30 08:15:37 +0200

    Commit, but this code actually worked first try

--- main.py
+++ main.py
@@ -7,17 +7,19 @@ sys.path.insert(0, os.path.realpath(os.path.dirname(__file__)) + '\\Source')
 # from Source.dbscanPointCloud import pointcloud_dbscan
 import Source.gridRansacModule as grm
 from Source.fileHandler import (
-    # convert_ply_to_las,
+    convert_ply_to_las,
     get_file_path,
     readout_LAS_file
 )
 from Source.pointCloudAltering import (
-    # combine_point_cloud,
     grid_subsampling,
     remove_noise_statistical
 )
 from Source.pointCloudEditor import open_point_cloud_editor
-from Source.shapeUtils import repair_point_cloud_module, transform_mesh_to_pcd
+from Source.shapeUtils import (
+    repair_point_cloud_module,
+    transform_mesh_to_pcd
+)
 
 if __name__ == "__main__":
     file_name = get_file_path("LAS and LAZ files", ["*.las", "*.laz"])
@@ -53,14 +55,11 @@ if __name__ == "__main__":
 
         open_point_cloud_editor(transformed_pcd, False)
 
+    # ____________________________ PLY MODULE ____________________________ # noqa: E303
+    # When the point cloud alterations are done, the point cloud is saved as a PLY file or no LAS file is given, this part will start.
+    print("\n Starting PLY module")
 
+    # Print a nice line over the width of the terminal
+    print(u'\u2500' * term_size.columns)
 
-
-    # # ____________________________ PLY MODULE ____________________________ # noqa: E303
-    # # When the point cloud alterations are done, the point cloud is saved as a PLY file or no LAS file is given, this part will start.
-    # print("\n Starting PLY module")
-
-    # # Print a nice line over the width of the terminal
-    # print(u'\u2500' * term_size.columns)
-
-    # convert_ply_to_las(file_name)
+    convert_ply_to_las(file_name)
