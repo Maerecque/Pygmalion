7ad15276778d56dd6da815ac4c1299b3f5b51eba
Marc Zoomers <marczoomers@gmail.com>
2023-08-31 09:44:48 +0200

    Added seperate application for resizing pointclouds
    Works in Python 3.9 and 3.7 🤠
    TO-DO: Add a progressbar for loading in files (Nice to have)

---
+++ loose_applications/pointcloud_resize/open3d_visualization.py
@@ -0,0 +1,24 @@
+import open3d as o3d
+import os
+
+
+def visualize_point_cloud(pcd_path):
+    point_cloud = o3d.io.read_point_cloud(pcd_path)
+    o3d.visualization.draw_geometries([point_cloud])
+
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) != 2:
+        print("Usage: python open3d_visualization.py <path_to_pcd>")
+        sys.exit(1)
+
+    pcd_path = sys.argv[1]
+    visualize_point_cloud(pcd_path)
+
+    # Close the temporary PCD file
+    if pcd_path.startswith("temp_"):
+        try:
+            os.remove(pcd_path)
+        except Exception as e:
+            print("An error occurred while deleting the temporary PCD file:", str(e))
