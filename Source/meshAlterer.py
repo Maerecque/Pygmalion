05ffd6f01ed2a5cae3649c3b186396dbb0ef2058
Marc Zoomers <marczoomers@gmail.com>
2024-07-29 09:44:34 +0200

    Fixed linting by Flake8 standards

--- Source/meshAlterer.py
+++ Source/meshAlterer.py
@@ -3,7 +3,6 @@ from matplotlib.colors import Normalize
 import numpy as np
 import open3d as o3d
 from scipy.ndimage import binary_dilation, binary_erosion
-import open3d as o3d
 from tqdm import tqdm
 
 
@@ -12,13 +11,13 @@ def mesh_simple_downsample(
     original_point_cloud: o3d.cpu.pybind.geometry.PointCloud,
     distance_threshold: float = 0.05,
     visualize_mesh: bool = False,
-    ) -> o3d.cpu.pybind.geometry.TriangleMesh:
+) -> o3d.cpu.pybind.geometry.TriangleMesh:
     """Simplifies a mesh by removing vertices that are far from the original point cloud.
 
     Args:
         mesh (o3d.cpu.pybind.geometry.TriangleMesh): The mesh to be simplified.
         original_point_cloud (o3d.cpu.pybind.geometry.PointCloud): The original point cloud.
-        distance_threshold (float, optional): The maximum distance between a vertex in the mesh and the nearest point in the point cloud. Defaults to 0.05.
+        distance_threshold (float, optional): The max distance between a vertex in the mesh and the nearest point in the pointcloud. Defaults to 0.05.
         visualize_mesh (bool, optional): Boolean to visualize the simplified mesh. Defaults to False.
 
     Raises:
@@ -30,16 +29,16 @@ def mesh_simple_downsample(
     # Check if inputs are not empty
     if not mesh:
         raise ValueError("The mesh is empty.")
-    
+
     if not original_point_cloud:
         raise ValueError("The point cloud is empty.")
-    
+
     # Print that the simplification process has started
     print("Starting mesh simplification...")
-    
+
     # Step 1: Get the number of triangles in the mesh
     triangle_amount = len(mesh.triangles)
-    
+
     # Step 2: Compute the distance from each vertex in the mesh to the nearest point in the point cloud
     o_pcd_tree = o3d.geometry.KDTreeFlann(original_point_cloud)
     distances = []
@@ -73,16 +72,16 @@ def mesh_simple_downsample(
     # Optional: Recompute normals for the new mesh
     new_mesh.compute_vertex_normals()
     new_mesh.compute_triangle_normals()
-    
+
     # Smoothing the mesh
     smooth_mesh = new_mesh.filter_smooth_simple(number_of_iterations=15)
-    
+
     if visualize_mesh:
         # Visualize the simplified mesh
         o3d.visualization.draw_geometries([smooth_mesh], mesh_show_back_face=True)
-    
+
     print(f"Removed {triangle_amount - len(smooth_mesh.triangles)} triangles")
-    
+
     return smooth_mesh
 
 
@@ -90,7 +89,8 @@ def transform_mesh_to_height_map(
     mesh: o3d.cpu.pybind.geometry.TriangleMesh,
     grid_size: int = 200,
     visualize_map: bool = False,
-    debugging_logs: bool = False):
+    debugging_logs: bool = False
+) -> o3d.cpu.pybind.geometry.PointCloud:
     """Transforms a mesh into a height map by projecting it onto the x-y plane.
 
     Args:
@@ -101,29 +101,29 @@ def transform_mesh_to_height_map(
 
     Returns:
         o3d.cpu.pybind.geometry.PointCloud: The point cloud representing the height map.
-        
+
     Raises:
         ValueError: If the mesh is empty.
-    """    
+    """
     # Check if mesh is empty
     if not mesh:
         raise ValueError("The mesh is empty.")
-    
+
     # Print that the transformation process has started
     print("Starting calculation of height map...")
-    
+
     # Print statistics of the mesh
     if debugging_logs:
         print("Mesh statistics:")
         print("Amount of vertices:", len(mesh.vertices))
         print("Amount of triangles:", len(mesh.triangles))
-        
+
     # Project mesh onto the x-y plane (use Z as height)
     vertices = np.asarray(mesh.vertices)
     x = vertices[:, 0]  # X-coordinates for positioning
     y = vertices[:, 1]  # Y-coordinates for positioning
     z = vertices[:, 2]  # Z-coordinates as height
-    
+
     if debugging_logs:
         # Debugging: Print min and max values
         print("x range:", x.min(), x.max())
@@ -138,27 +138,26 @@ def transform_mesh_to_height_map(
     x_grid = np.linspace(x_min, x_max, grid_size)
     y_grid = np.linspace(y_min, y_max, grid_size)
     X, Y = np.meshgrid(x_grid, y_grid)
-    
+
     # Show the amount of points in the grid
     if debugging_logs:
         print("Amount of points in the grid:", len(X.flatten()))
-    
-    
+
     # Keep per x-y coordinate the highest z value
     height_map = np.full((grid_size, grid_size), -np.inf)
     for i in tqdm(range(len(x)), desc="Creating height map"):
         x_idx = np.argmin(np.abs(x_grid - x[i]))
         y_idx = np.argmin(np.abs(y_grid - y[i]))
         height_map[x_idx, y_idx] = max(height_map[x_idx, y_idx], z[i])
-        
+
     # So currently there can still be some holes in the height map, can be looked at later.
-    
+
     # Visualize the height map
     if visualize_map:
         plt.imshow(height_map, cmap="viridis", norm=Normalize(vmin=z_min, vmax=z_max))
         plt.colorbar()
         plt.show()
-        
+
     if debugging_logs:
         # Show statistics of the height map
         print("Height map statistics:")
@@ -169,14 +168,14 @@ def transform_mesh_to_height_map(
         print("Amount of invalid points:", np.sum(height_map == -np.inf))
         print("Amount of total points:", grid_size * grid_size)
         print("Percentage of valid points:", np.sum(height_map != -np.inf) / (grid_size * grid_size) * 100, "%")
-    
+
     # Transform the height map into an open3d point cloud to get an idea of the floorplan
-    print("Creating the floor plan  point cloud...")    
+    print("Creating the floor plan  point cloud...")
     floor_plan_points = np.argwhere(height_map != -np.inf)
     floor_plan_coords = np.array([x_grid[floor_plan_points[:, 1]], y_grid[floor_plan_points[:, 0]], np.full(floor_plan_points.shape[0], z_min)]).T
     floor_plan_point_cloud = o3d.geometry.PointCloud()
     floor_plan_point_cloud.points = o3d.utility.Vector3dVector(floor_plan_coords)
-    
+
     # Create the same pointcloud but with correct z values
     print("Creating the ceiling point cloud...")
     ceiling_points = np.argwhere(height_map != -np.inf)
@@ -184,18 +183,18 @@ def transform_mesh_to_height_map(
     ceiling_coords = np.array([x_grid[ceiling_points[:, 1]], y_grid[ceiling_points[:, 0]], z_values]).T
     ceiling_point_cloud = o3d.geometry.PointCloud()
     ceiling_point_cloud.points = o3d.utility.Vector3dVector(ceiling_coords)
-    
+
     # Calculate the point density of the floor plan
     point_density = len(floor_plan_coords) / (grid_size * grid_size)
 
     if debugging_logs:
         print("Point density of the floor plan:", point_density)
-    
+
     # Find the edges of the floor plan and ceiling based their x and y coordinates
     print("Finding the edges of the floor plan and ceiling...")
     floor_plan_edges = np.argwhere(binary_dilation(height_map != -np.inf) ^ binary_erosion(height_map != -np.inf))
     ceiling_edges = np.argwhere(binary_dilation(height_map != -np.inf) ^ binary_erosion(height_map != -np.inf))
-    
+
     # Visualize the floor plan and ceiling edges in a 3d plot
     if visualize_map:
         fig = plt.figure()
@@ -203,7 +202,7 @@ def transform_mesh_to_height_map(
         ax.scatter(floor_plan_edges[:, 1], floor_plan_edges[:, 0], np.full(floor_plan_edges.shape[0], z_min), c='r', marker='o')
         ax.scatter(ceiling_edges[:, 1], ceiling_edges[:, 0], height_map[ceiling_edges[:, 0], ceiling_edges[:, 1]], c='b', marker='o')
         plt.show()
-    
+
     # Create walls between floor edges and ceiling edges
     wall_points = []
 
@@ -227,7 +226,7 @@ def transform_mesh_to_height_map(
         wall_point_cloud.points = o3d.utility.Vector3dVector(wall_points)
     else:
         wall_point_cloud = o3d.geometry.PointCloud()
-    
+
     # Combine the floor plan and ceiling point cloud
     hull_points = np.concatenate([
         np.asarray(floor_plan_point_cloud.points),
@@ -236,7 +235,7 @@ def transform_mesh_to_height_map(
     ])
     hull_point_cloud = o3d.geometry.PointCloud()
     hull_point_cloud.points = o3d.utility.Vector3dVector(hull_points)
-    
+
     # Visualize the point cloud
     if visualize_map:
         o3d.visualization.draw_geometries([hull_point_cloud])
